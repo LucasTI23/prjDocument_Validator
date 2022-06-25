d7046439dca20f88010c8bdf2ddc0a14e273408f
Lucas Silva de Faria <87736324+LucasTI23@users.noreply.github.com>
2022-06-24 21:55:59 -0300

    Add files via upload

---
+++ VALIDADOR_DE_DOCUMENTO/VALIDADOR_DE_DOCUMENTO/API_PESSOA/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_PESSOA
+{
+    public abstract class Pessoa
+    {
+        public long Codigo { get; set; }
+        public string Nome { get; set; }
+
+        public Pessoa (long Codigo, string Nome)
+        {
+            this.Codigo = Codigo;
+            this.Nome = Nome;
+        }
+
+        public abstract string Imprimir();
+        public abstract bool Validar();
+    }
+}
---
+++ VALIDADOR_DE_DOCUMENTO/VALIDADOR_DE_DOCUMENTO/API_PESSOA/PessoaFisica.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_PESSOA
+{
+    public class PessoaFisica: Pessoa
+    {
+        public long CPF { get; set; }
+
+        public PessoaFisica(long codigo, string Nome, long CPF)
+            : base(codigo, Nome)
+        {
+            this.CPF = CPF;
+        }
+
+        public override string Imprimir()
+        {
+            string valor = String.Format("COD:{0}\tNOME:{1}\tDOC:{2}", Codigo, Nome, CPF);
+            return valor;
+        }
+
+        public override bool Validar()
+        {
+            char[] digitos = CPF.ToString().ToCharArray();
+            int peso = 10;
+            int soma = 0;
+            int div1 = 0,
+                div2 = 0,
+                resto;
+            for (int i = 0; i < 9; i++)
+            {
+                soma += peso * Convert.ToInt16(digitos[i].ToString());
+                peso--;
+            }
+            resto = soma % 11;
+            if (resto < 2) div1 = 0; else div1 = 11 - resto;
+            if (div1 != Convert.ToInt16(digitos[9].ToString()))
+                return false;
+            peso = 11;
+            resto = 0;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                soma += peso * Convert.ToInt16(digitos[i].ToString());
+                peso--;
+            }
+            resto = soma % 11;
+            if (resto < 2) div2 = 0; else div2 = 11 - resto;
+            if (div2 != Convert.ToInt16(digitos[10].ToString())) return false;
+            return true;
+        }
+    }
+}
